4bd6d5822654c7a61bb74e345a4e020c7e0dbd7c
HAhsCS-4me <67917692+HAhsCS-4me@users.noreply.github.com>
2022-02-16 09:02:09 -0700

    Update spellcheck.py

--- spellcheck.py
+++ spellcheck.py
@@ -14,7 +14,33 @@ def main():
     # Print first 50 values of each list to verify contents
     print(dictionary[0:50])
     print(aliceWords[0:50])
+
+    selection = getSelection()
+
+    loop = True
+    while loop:
+        if selection == '1':
+            linearSpell(dictionary)
+        #elif selection == '2':
+            #binarySpell(dictionary)
+        #elif selection == '3':
+        #    x = 1
+        #elif selection == '4':
+        #    x = 2
+        #elif selection == '5':
+        #    loop = False
 # end main()
+
+def getSelection():
+    print('\nMain Menu')
+    print('1: Spell Check a Word (Linear Search)')
+    print('2: Spell Check a Word (Binary Search)')
+    print('3: Spell Check Alice In Wonderland (Linear Search)')
+    print('4: Spell Check Alice In Wonderland (Binary Search)')
+    print('5: Exit')
+
+    return input('Enter menu selection (1-5): ')
+    return input('')
 
 
 def loadWordsFromFile(fileName):
@@ -26,6 +52,10 @@ def loadWordsFromFile(fileName):
     # Split text by one or more whitespace characters
     return re.split('\s+', textData)
 # end loadWordsFromFile()
+
+def linearSpell(array):
+    
+#def binarySpell():
 
 
 # Call main() to begin program
