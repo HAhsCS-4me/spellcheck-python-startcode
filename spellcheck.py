688a9ba0aecc9eced4c644f18a465fcb1ba8755b
HAhsCS-4me <67917692+HAhsCS-4me@users.noreply.github.com>
2022-02-17 09:21:43 -0700

    linear and binary searches good to go

--- spellcheck.py
+++ spellcheck.py
@@ -15,19 +15,19 @@ def main():
     print(dictionary[0:50])
     print(aliceWords[0:50])
 
-    selection = getSelection()
-    word = spellCheck()
-
     loop = True
     while loop:
+        selection, word = getSelection()
         if selection == '1':
-            linearSearch(dictionary, word)
+            s = 'hery'
+            #linearSearch(dictionary, word)
         elif selection == '2':
             binarySearch(dictionary, word)
         elif selection == '3':
-            linearSearch(aliceWords)    
+            b = 'jem'
+            #linearSearch(aliceWords)    
         elif selection == '4':
-            binarySearch(aliceWords)
+            binarySearch(aliceWords, word)
         elif selection == '5':
             loop = False
 # end main()
@@ -40,7 +40,10 @@ def getSelection():
     print('4: Spell Check Alice In Wonderland (Binary Search)')
     print('5: Exit')
 
-    return input('Enter menu selection (1-5): ')
+    selection = input('Enter menu selection (1-5): ')
+    word = input('Please enter a word: ')
+    
+    return selection, word
 # end of getSelection()
 
 def loadWordsFromFile(fileName):
@@ -53,30 +56,27 @@ def loadWordsFromFile(fileName):
     return re.split('\s+', textData)
 # end loadWordsFromFile()
 
-# Get desired word
-def spellCheck():
-    return input('Please enter a word: ')
-
 # Linear Searches
-# def linearSearch(array, item):
-    
-# end of linearSearch()
+def linearSearch(anArray, item):
+    for x in range(0, len(anArray)):
+        if anArray[x] == item:
+            return x
+    return -1 
+#end of linearSearch()
 
 # Binary Searches
 def binarySearch(array, item):
     ui = len(array) - 1
     li = 0
 
-    i = 0
-    while i < len(array):
-        mi = int(((ui + li) / 2))
+    while ui >= li:
+        mi = (ui + li) // 2
         if item == array[mi]:
             return mi
         elif item < array[mi]:
             ui = mi - 1
-        elif item > array[mi]:
+        else:
             li = mi + 1
-        i += 1
     return -1
 # end of binarySearch()
 
